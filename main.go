39d2937b353ff538b8b9f715beb02553c0e5c10b
kkdosk <kkdosk@naver.com>
2018-01-15 23:37:04 +0900

    main modified

--- main.go
+++ main.go
@@ -14,9 +14,16 @@ func main() {
 		})
 	})
 
-	r.GET("/user/:name", func(c *gin.Conext) {
-		name := r.Param("name")
+	r.GET("/user/:name", func(c *gin.Context) {
+		name := c.Param("name")
 		c.String(http.StatusOK, "Hello %s", name)
+	})
+
+	r.GET("/user/:name/*action", func(c *gin.Context) {
+		name := c.Param("name")
+		action := c.Param("action")
+		message := name + " is " + action
+		c.String(http.StatusOK, message)
 	})
 	r.Run(":8081") // listen and serve on 0.0.0.0:8080
 }
